691b565b0a4929389b665788bd17deafefbee526
maabuu <42468905+maabuu@users.noreply.github.com>
2023-06-06 10:19:01 +0100

    Documenting

--- molbuster/tools/logging.py
+++ molbuster/tools/logging.py
@@ -16,6 +16,7 @@ class CaptureLogger(logging.Handler):
     """Helper class that captures Python logger output."""
 
     def __init__(self, module=None):
+        """Initialize logger."""
         super().__init__(level=logging.NOTSET)
         self.logs = {}
         self.devnull = open(os.devnull, "w")
@@ -23,18 +24,22 @@ class CaptureLogger(logging.Handler):
         rdkit.logger.addHandler(self)
 
     def __enter__(self):
+        """Enter context manager."""
         return self.logs
 
     def __exit__(self, *args):
+        """Exit context manager."""
         self.release()
 
     def handle(self, record):
+        """Handle log record."""
         key = record.levelname
         val = self.format(record)
         self.logs[key] = self.logs.get(key, "") + val
         return False
 
     def release(self):
+        """Release logger."""
         rdkit.log_handler.setStream(sys.stderr)
         rdkit.logger.removeHandler(self)
         self.devnull.close()
