e35bad74eb48faea7b32027b6e4d67b0502e4425
maabuu <42468905+maabuu@users.noreply.github.com>
2025-10-15 15:13:21 +0100

    Add test

--- tests/test_modules/test_distance_geometry.py
+++ tests/test_modules/test_distance_geometry.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from rdkit.Chem.rdmolfiles import MolFromPDBFile
+
 from posebusters.modules.distance_geometry import _two_bonds_to_angle, check_geometry
 
 
@@ -18,3 +20,12 @@ def test_two_bonds_to_angle():
     assert _two_bonds_to_angle((1, 0), (1, 2)) == (0, 1, 2)
     assert _two_bonds_to_angle((1, 0), (2, 1)) == (0, 1, 2)
     assert _two_bonds_to_angle((0, 1), (2, 3)) is None
+
+
+def test_molecule_issue_80():
+    """Test sanitizable molecule from issue 80 that causes a RunTime error in GetMoleculeBoundsMatrix."""
+
+    mol = MolFromPDBFile("tests/conftest/mol_issue_80.pdb", removeHs=False, sanitize=True)
+    assert mol is not None
+    # should not raise an error
+    check_geometry(mol)
