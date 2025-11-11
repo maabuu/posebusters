c6c4339d0a807804802a4054fbca3d36bdc98738
maabuu <42468905+maabuu@users.noreply.github.com>
2025-11-11 13:38:53 +0000

    Update tests as molecules will have hydrogens added if needed

--- tests/test_modules/test_energy_ratio.py
+++ tests/test_modules/test_energy_ratio.py
@@ -13,7 +13,7 @@ def test_check_energy_ratio(mol_pm2):
     assert math.isfinite(out["results"]["mol_pred_energy"])
     assert math.isfinite(out["results"]["ensemble_avg_energy"])
     assert math.isfinite(out["results"]["energy_ratio"])
-    assert out["results"]["energy_ratio_passes"] is True
+    assert out["results"]["energy_ratio_passes"]
 
 
 def test_check_energy_ratio_14gs_0(mol_pred_14gs_gen0):
@@ -22,43 +22,43 @@ def test_check_energy_ratio_14gs_0(mol_pred_14gs_gen0):
     assert math.isfinite(out["results"]["mol_pred_energy"])
     assert math.isfinite(out["results"]["ensemble_avg_energy"])
     assert math.isfinite(out["results"]["energy_ratio"])
-    assert out["results"]["energy_ratio_passes"] is True
+    assert out["results"]["energy_ratio_passes"]
 
 
 def test_check_energy_ratio_1afs_87(mol_pred_1afs_gen87):
-    # nan because molecule cannot be converted to valid InChI
+    # molecule can be sanitized and hydrogens can be added, conformation not horrible
     out = check_energy_ratio(mol_pred_1afs_gen87)
-    assert math.isnan(out["results"]["mol_pred_energy"])
-    assert math.isnan(out["results"]["ensemble_avg_energy"])
-    assert math.isnan(out["results"]["energy_ratio"])
-    assert math.isnan(out["results"]["energy_ratio_passes"])
+    assert math.isfinite(out["results"]["mol_pred_energy"])
+    assert math.isfinite(out["results"]["ensemble_avg_energy"])
+    assert math.isfinite(out["results"]["energy_ratio"])
+    assert out["results"]["energy_ratio_passes"]
 
 
 def test_check_energy_ratio_1afs_94(mol_pred_1afs_gen94):
     # molecule is chemically insane, but conformation looks alright
     out = check_energy_ratio(mol_pred_1afs_gen94)
     assert math.isfinite(out["results"]["mol_pred_energy"])
-    assert math.isnan(out["results"]["ensemble_avg_energy"])
-    assert math.isnan(out["results"]["energy_ratio"])
-    assert math.isnan(out["results"]["energy_ratio_passes"])
+    assert math.isfinite(out["results"]["ensemble_avg_energy"])
+    assert math.isfinite(out["results"]["energy_ratio"])
+    assert out["results"]["energy_ratio_passes"]
 
 
 def test_check_energy_ratio_1jn2_3(mol_pred_1jn2_gen3):
-    # nan because molecule cannot be converted to valid InChI
+    # molecule can be sanitized and hydrogens can be added, conformation not horrible
     out = check_energy_ratio(mol_pred_1jn2_gen3)
-    assert math.isnan(out["results"]["mol_pred_energy"])
-    assert math.isnan(out["results"]["ensemble_avg_energy"])
-    assert math.isnan(out["results"]["energy_ratio"])
-    assert math.isnan(out["results"]["energy_ratio_passes"])
+    assert math.isfinite(out["results"]["mol_pred_energy"])
+    assert math.isfinite(out["results"]["ensemble_avg_energy"])
+    assert math.isfinite(out["results"]["energy_ratio"])
+    assert out["results"]["energy_ratio_passes"]
 
 
 def test_check_energy_ratio_1jn2_62(mol_pred_1jn2_gen62):
     # molecule is chemically insane, but conformation looks alright
     out = check_energy_ratio(mol_pred_1jn2_gen62)
     assert math.isfinite(out["results"]["mol_pred_energy"])
-    # assert math.isfinite(out["results"]["ensemble_avg_energy"])
-    # assert math.isfinite(out["results"]["energy_ratio"])
-    # assert out["results"]["energy_ratio_passes"] is True
+    assert math.isfinite(out["results"]["ensemble_avg_energy"])
+    assert math.isfinite(out["results"]["energy_ratio"])
+    assert out["results"]["energy_ratio_passes"]
 
 
 def test_check_energy_ratio_approximate_consistency(mol_1a30_clash_2, mol_1a30_clash_3):
