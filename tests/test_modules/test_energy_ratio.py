4e76dde5780018bde2aacc9f2dbaa16c80bfa8b3
maabuu <42468905+maabuu@users.noreply.github.com>
2026-09-15 06:10:44 +0200

    Update tests for rdkit 2026

--- tests/test_modules/test_energy_ratio.py
+++ tests/test_modules/test_energy_ratio.py
@@ -4,12 +4,12 @@ import math
 
 import numpy as np
 
-from posebusters.modules.energy_ratio import check_energy_ratio
+from posebusters.modules import energy_ratio
 
 
 def test_check_energy_ratio(mol_pm2):
     # molecule has valid conformation
-    out = check_energy_ratio(mol_pm2)
+    out = energy_ratio.check_energy_ratio(mol_pm2)
     assert math.isfinite(out["results"]["mol_pred_energy"])
     assert math.isfinite(out["results"]["ensemble_avg_energy"])
     assert math.isfinite(out["results"]["energy_ratio"])
@@ -18,7 +18,7 @@ def test_check_energy_ratio(mol_pm2):
 
 def test_check_energy_ratio_14gs_0(mol_pred_14gs_gen0):
     # molecule is chemically insane, but conformation looks alright
-    out = check_energy_ratio(mol_pred_14gs_gen0, threshold_energy_ratio=10.0)
+    out = energy_ratio.check_energy_ratio(mol_pred_14gs_gen0, threshold_energy_ratio=10.0)
     assert math.isfinite(out["results"]["mol_pred_energy"])
     assert math.isfinite(out["results"]["ensemble_avg_energy"])
     assert math.isfinite(out["results"]["energy_ratio"])
@@ -27,7 +27,7 @@ def test_check_energy_ratio_14gs_0(mol_pred_14gs_gen0):
 
 def test_check_energy_ratio_1afs_87(mol_pred_1afs_gen87):
     # molecule can be sanitized and hydrogens can be added, conformation not horrible
-    out = check_energy_ratio(mol_pred_1afs_gen87)
+    out = energy_ratio.check_energy_ratio(mol_pred_1afs_gen87)
     assert math.isfinite(out["results"]["mol_pred_energy"])
     assert math.isfinite(out["results"]["ensemble_avg_energy"])
     assert math.isfinite(out["results"]["energy_ratio"])
@@ -36,7 +36,7 @@ def test_check_energy_ratio_1afs_87(mol_pred_1afs_gen87):
 
 def test_check_energy_ratio_1afs_94(mol_pred_1afs_gen94):
     # molecule is chemically insane, but conformation looks alright
-    out = check_energy_ratio(mol_pred_1afs_gen94)
+    out = energy_ratio.check_energy_ratio(mol_pred_1afs_gen94)
     assert math.isfinite(out["results"]["mol_pred_energy"])
     assert math.isfinite(out["results"]["ensemble_avg_energy"])
     assert math.isfinite(out["results"]["energy_ratio"])
@@ -45,16 +45,27 @@ def test_check_energy_ratio_1afs_94(mol_pred_1afs_gen94):
 
 def test_check_energy_ratio_1jn2_3(mol_pred_1jn2_gen3):
     # molecule can be sanitized and hydrogens can be added, conformation not horrible
-    out = check_energy_ratio(mol_pred_1jn2_gen3)
+    out = energy_ratio.check_energy_ratio(mol_pred_1jn2_gen3)
     assert math.isfinite(out["results"]["mol_pred_energy"])
     assert math.isfinite(out["results"]["ensemble_avg_energy"])
     assert math.isfinite(out["results"]["energy_ratio"])
     assert out["results"]["energy_ratio_passes"]
 
 
-def test_check_energy_ratio_1jn2_62(mol_pred_1jn2_gen62):
+def test_check_energy_ratio_1jn2_62(mol_pred_1jn2_gen62, monkeypatch):
     # molecule is chemically insane, but conformation looks alright
-    out = check_energy_ratio(mol_pred_1jn2_gen62)
+    etkdg_config = energy_ratio.ETKDGv3
+
+    # disable because rdkit flags this as an atropisomer which etkdg cannot provide enough conformations for
+    def etkdg_without_chirality(*args, **kwargs):
+        params = etkdg_config(*args, **kwargs)
+        params.enforceChirality = False
+        return params
+
+    monkeypatch.setattr(energy_ratio, "ETKDGv3", etkdg_without_chirality)
+    energy_ratio.get_energies.cache_clear()
+    out = energy_ratio.check_energy_ratio(mol_pred_1jn2_gen62)
+    energy_ratio.get_energies.cache_clear()
     assert math.isfinite(out["results"]["mol_pred_energy"])
     assert math.isfinite(out["results"]["ensemble_avg_energy"])
     assert math.isfinite(out["results"]["energy_ratio"])
@@ -62,8 +73,8 @@ def test_check_energy_ratio_1jn2_62(mol_pred_1jn2_gen62):
 
 
 def test_check_energy_ratio_approximate_consistency(mol_1a30_clash_2, mol_1a30_clash_3):
-    energy_2 = check_energy_ratio(mol_1a30_clash_2)["results"]["mol_pred_energy"]
-    energy_3 = check_energy_ratio(mol_1a30_clash_3)["results"]["mol_pred_energy"]
+    energy_2 = energy_ratio.check_energy_ratio(mol_1a30_clash_2)["results"]["mol_pred_energy"]
+    energy_3 = energy_ratio.check_energy_ratio(mol_1a30_clash_3)["results"]["mol_pred_energy"]
 
     # check numbers are approximately equal
     assert np.isclose(energy_2, energy_3)
@@ -71,7 +82,7 @@ def test_check_energy_ratio_approximate_consistency(mol_1a30_clash_2, mol_1a30_c
 
 def test_check_energy_ratio_disconnected_atoms(mol_disconnnected_atoms):
     # no bonds, just disconnected atoms -> energy is 0
-    out = check_energy_ratio(mol_disconnnected_atoms)
+    out = energy_ratio.check_energy_ratio(mol_disconnnected_atoms)
     assert math.isfinite(out["results"]["mol_pred_energy"])
     assert math.isfinite(out["results"]["ensemble_avg_energy"])
     assert math.isfinite(out["results"]["energy_ratio"])
@@ -80,5 +91,5 @@ def test_check_energy_ratio_disconnected_atoms(mol_disconnnected_atoms):
 
 def test_check_energy_ratio_iron_ion(mol_fe2plus):
     # test fails because UFF does not have parameters for Fe 2+ ion
-    out = check_energy_ratio(mol_fe2plus)
+    out = energy_ratio.check_energy_ratio(mol_fe2plus)
     assert math.isnan(out["results"]["energy_ratio_passes"])
